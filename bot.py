ba20270290b0d39ee266eb2fb6b81542fdbd0f91
pavel.boykov <pavel.boykov@rubius.com>
2022-05-28 08:38:44 +0000

    Fixed gitignore

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 from aiogram import Bot, Dispatcher, executor, types
+
 from auth_data import ML_ALERT_BOT_TOKEN
+
 
 bot = Bot(token=ML_ALERT_BOT_TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
     await message.reply("Hi!")
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
